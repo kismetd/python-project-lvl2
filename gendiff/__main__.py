f01d533a54fa0fc13367bb9ed59d6d7cf8fbbf4f
kismetd <hlevnmaxim@gmail.com>
2022-08-06 14:33:58 +0300

    feat: add argparse support

--- gendiff/__main__.py
+++ gendiff/__main__.py
@@ -1,5 +1,24 @@
+#!/usr/bin/env python3
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="gendiff",
+    description="Compares two configuration files and shows a difference.",
+)
+parser.add_argument(
+    "-f",
+    "--format",
+    help="set format of output (default: ...)",
+    # choices=,
+    # default=,
+)
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+args = parser.parse_args()
+
+
 def main():
-    print("In main...")
+    print("Run generate_diff({args.first_file, args.second_file, args.format)")
 
 
 if __name__ == "__main__":
