444fc479f5383cb91c32db6ab9f0908f3dcfd1d8
kismetd <hlevnmaxim@gmail.com>
2022-08-07 14:27:14 +0300

    tests: test tool functionality for flat json and yaml

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,5 +1,23 @@
-from gendiff import __version__
+from pathlib import Path
+
+import pytest
+from conftest import get_fixture_path
+from gendiff.lib import generate_diff
 
 
-def test_initial():
-    assert __version__ == "0.1.0"
+@pytest.mark.parametrize(
+    "file_1, file_2, type, output, formatter",
+    [
+        ("file1.json", "file2.json", "flat", "flat-diff.txt", "stylish"),
+        ("file1.yaml", "file2.yaml", "flat", "flat-diff.txt", "stylish"),
+        ("file1.yml", "file2.yml", "flat", "flat-diff.txt", "stylish"),
+        ("file1.yml", "file2.yaml", "flat", "flat-diff.txt", "stylish"),
+    ],
+)
+def test_flat_files_stylish(file_1, file_2, type, output, formatter):
+    file_1 = get_fixture_path(file_1, type)
+    file_2 = get_fixture_path(file_2, type)
+    diff_path = Path(get_fixture_path(output, formatter, True))
+    with diff_path.open() as expected:
+        expected = diff_path.read_text()
+    assert generate_diff(file_1, file_2, formatter) == expected
