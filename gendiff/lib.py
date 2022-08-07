72d2309f575be804acc3d7116c934074b959da55
kismetd <hlevnmaxim@gmail.com>
2022-08-07 15:51:14 +0300

    fix: style arg is used as supposed to

--- gendiff/lib.py
+++ gendiff/lib.py
@@ -1,13 +1,15 @@
 from gendiff.diff_log import generate_log
 from gendiff.parser import parse_files
+from gendiff.renderers.log_like import parse_log
 from gendiff.renderers.stylish import stylish
 
 RENDERERS = {
     "stylish": stylish,
+    "log-like": parse_log,
 }
 
 
 def generate_diff(file_1, file_2, style="stylish"):
     file_1, file_2 = parse_files(file_1, file_2)
     diff_log = generate_log(file_1, file_2)
-    return RENDERERS["stylish"](diff_log)
+    return RENDERERS[style](diff_log)
