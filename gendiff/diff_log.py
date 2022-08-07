d8dba218e3962cb0fa54ff1e41501a30dc282869
kismetd <hlevnmaxim@gmail.com>
2022-08-07 08:39:57 +0300

    refactor: change return value of generate_log()
    generate_log() used to return a structure of form:
      1. {"property": x, "val1": a} - for unchanged, removed and added
        properties.
      2. {"property": x, "val1": a, "val2": b} - for changed properties.
    
    Now generate_log() returns a structure of form:
      {"property": x, values:{val1: a, ...}}
    Where all values (2 for changed property and 1 for other) are stored in
    a separate dict inside the outer dict.
    
    Functions that process the output of generate_log() in any way were
    also changed accordingly.

--- gendiff/diff_log.py
+++ gendiff/diff_log.py
@@ -1,53 +1,48 @@
 def generate_log(file_1, file_2):
     log = []
+
     for key, value in file_1.items():
+        prop = {"property": key}
         if file_2.get(key):
             if value == file_2[key]:
-                log.append({"property": key, "unchanged": str(value)})
+                prop["values"] = {"unchanged": str(value)}
             else:
-                log.append(
-                    {
-                        "property": key,
-                        "old": str(value),
-                        "new": str(file_2[key]),
-                    }
-                )
+                prop["values"] = {"old": str(value), "new": str(file_2[key])}
         else:
-            log.append({"property": key, "old": str(value)})
-    # check file_2 for values that were not present in file_1
+            prop["values"] = {"old": str(value)}
+        log.append(prop)
+
     for key, value in file_2.items():
         if not file_1.get(key):
-            log.append({"property": key, "new": str(value)})
-    # sort in alphanumeric order by property name
+            prop = {"property": key}
+            prop["values"] = {"new": str(value)}
+            log.append(prop)
+
     log.sort(key=lambda x: x["property"])
     return log
 
 
-def _parse_property(entry, stat):
+def _parse_property(prop, vals, stat):
     val_stat = stat
     if stat == "removed":
         val_stat = "old"
     elif val_stat == "added":
         val_stat = "new"
-    prpt = entry["property"]
     t = "flat"
-    val = (
-        "complex" if isinstance(entry[val_stat], dict) else entry[val_stat]
-    )  # noqa E501
+    val = "complex" if isinstance(vals[val_stat], dict) else vals[val_stat]
     if val == "complex":
         t = "nested"
-    return f"Property: [{prpt}]. Type: [{t}]. Status: [{stat}]. Value: [{val}]"
+    return f"Property: [{prop}]. Type: [{t}]. Status: [{stat}]. Value: [{val}]"
 
 
-def _parse_changed_property(entry):
-    prpt = entry["property"]
+def _parse_changed_property(prop, vals):
     t = "flat"
-    val_1 = "complex" if isinstance(entry["old"], dict) else entry["old"]
-    val_2 = "complex" if isinstance(entry["new"], dict) else entry["new"]
+    val_1 = "complex" if isinstance(vals["old"], dict) else vals["old"]
+    val_2 = "complex" if isinstance(vals["new"], dict) else vals["new"]
     if val_1 == "complex" or val_2 == "complex":
         t = "nested"
     return (
-        f"Property: [{prpt}]. Type: [{t}]."
+        f"Property: [{prop}]. Type: [{t}]."
         f" Status: [changed]. Value: was=[{val_1}], now=[{val_2}]"
     )
 
@@ -55,12 +50,14 @@ def _parse_changed_property(entry):
 def parse_log(log):
     outp = []
     for entry in log:
-        if entry.get("old") and entry.get("new"):
-            outp.append(_parse_changed_property(entry))
-        elif entry.get("old") and not entry.get("new"):
-            outp.append(_parse_property(entry, "removed"))
-        elif entry.get("new") and not entry.get("old"):
-            outp.append(_parse_property(entry, "added"))
-        elif entry.get("unchanged"):
-            outp.append(_parse_property(entry, "unchanged"))
+        prop = entry["property"]
+        vals = entry["values"]
+        if vals.get("old") and vals.get("new"):
+            outp.append(_parse_changed_property(prop, vals))
+        elif vals.get("old") and not vals.get("new"):
+            outp.append(_parse_property(prop, vals, "removed"))
+        elif vals.get("new") and not vals.get("old"):
+            outp.append(_parse_property(prop, vals, "added"))
+        elif vals.get("unchanged"):
+            outp.append(_parse_property(prop, vals, "unchanged"))
     return "\n".join(outp)
