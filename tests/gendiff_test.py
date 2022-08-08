ed672fe759aa1826e43a9fc7bd41795b8139ee50
kismetd <hlevnmaxim@gmail.com>
2022-08-08 11:12:05 +0300

    tests: tests for nested files

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -14,7 +14,23 @@ from gendiff.lib import generate_diff
         ("file1.json", "file2.json", "flat", "flat-diff.txt", "log-like"),
     ],
 )
-def test_flat_files_stylish(file_1, file_2, type, output, formatter):
+def test_flat_files(file_1, file_2, type, output, formatter):
+    file_1 = get_fixture_path(file_1, type)
+    file_2 = get_fixture_path(file_2, type)
+    diff_path = Path(get_fixture_path(output, formatter, True))
+    with diff_path.open() as expected:
+        expected = diff_path.read_text()
+    assert generate_diff(file_1, file_2, formatter) == expected
+
+
+@pytest.mark.parametrize(
+    "file_1, file_2, type, output, formatter",
+    [
+        ("file1.json", "file2.json", "nested", "nested-diff.txt", "stylish"),
+        ("file1.yaml", "file2.yaml", "nested", "nested-diff.txt", "stylish"),
+    ],
+)
+def test_nested_files(file_1, file_2, type, output, formatter):
     file_1 = get_fixture_path(file_1, type)
     file_2 = get_fixture_path(file_2, type)
     diff_path = Path(get_fixture_path(output, formatter, True))
