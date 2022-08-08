e75c3a0b991ab160a3e61bdcde30108206122b21
kismetd <hlevnmaxim@gmail.com>
2022-08-08 11:11:22 +0300

    feat: stylish format rendering for flat and nested JSON and YAML files

--- gendiff/renderers/stylish.py
+++ gendiff/renderers/stylish.py
@@ -1,23 +1,60 @@
-_VAL_STATUS = {
-    "unchanged": "  ",
-    "old": "- ",
-    "new": "+ ",
-}
+from itertools import chain
 
-_CONVERSION = {
-    "False": "false",
-    "True": "true",
-    "None": "null",
-}
+from .utils import STATUS_TABLE, parse_val
+
+SPACE = " "
 
 
-def stylish(log, indent="  "):
-    diff = []
-    for entry in log:
-        prop = entry["property"]
-        for key, val in entry["values"].items():
-            status = _VAL_STATUS[key]
-            if _CONVERSION.get(val):
-                val = _CONVERSION[val]
-            diff.append(f"{indent}{status}{prop}: {val}")
-    return "\n".join(["{"] + diff + ["}"])
+def format_value(tree, spaces=2):
+    step = 4
+
+    def _walk(node, depth):
+        if not isinstance(node, dict):
+            return parse_val(node)
+
+        line = []
+
+        for key, value in node.items():
+            indent = SPACE * (depth + step)
+            val = _walk(value, depth + step)
+            line.append(f"{indent}{key}: {val}")
+        result = chain("{", line, [depth * SPACE + "}"])
+        return "\n".join(result)
+
+    return _walk(tree, spaces)
+
+
+def form_line(key, value, stat, indent):
+    sign = STATUS_TABLE[stat]
+    return f"{indent * SPACE}{sign}{key}: {value}"
+
+
+def format(tree, spaces=2):
+    step = 4
+    inner_step = step // 2
+
+    def _walk(node, depth):
+        lines = []
+
+        for key in sorted(node):
+            prop = node[key]
+            stat = prop["status"]
+            value = prop.get("value")
+
+            if stat == "nested":
+                value = format(value, depth + step)
+
+            if stat == "changed":
+                old_value = format_value(prop["old"], depth + inner_step)
+                lines.append(form_line(key, old_value, "removed", depth))
+                new_value = format_value(prop["new"], depth + inner_step)
+                lines.append(form_line(key, new_value, "added", depth))
+                continue
+
+            value = format_value(value, depth + inner_step)
+            lines.append(form_line(key, value, stat, depth))
+
+        result = chain("{", lines, [(depth - inner_step) * SPACE + "}"])
+        return "\n".join(result)
+
+    return _walk(tree, spaces)
