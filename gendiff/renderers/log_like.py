9fa6fb0bce5a2c2d471d72cca0a6748e3b7a3e5b
kismetd <hlevnmaxim@gmail.com>
2022-08-08 11:37:15 +0300

    refactor: remove log_like format, similar format will be implemented as 'plain'

--- gendiff/renderers/log_like.py
+++
@@ -1,8 +0,0 @@
-def parse_log(log):
-    lines = []
-    for prop in sorted(log):
-        value = log[prop]
-        if value["status"] == "nested":
-            value = "complex"
-        lines.append(f"[Property: {prop}] = {value}")
-    return "\n".join(lines)
